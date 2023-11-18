f6adc829bf91857395af0e9a506c87bf286a3a0c
UHa <https00200@gmail.com>
2023-11-18 14:56:38 +0900

    🎨 Modifying Layout Style

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,27 @@
+import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import styled from 'styled-components';
+
+const LayoutLayout = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+const LayoutBox = styled.div`
+    border: 4px solid #bde5ea;
+    border-radius: 20px;
+    display: flex;
+    width: 90%;
+    height: 90%;
+`;
+
 export default function Layout() {
     return (
-        <>
-            <div>sidebar</div>
-            <div>content</div>
-        </>
+        <LayoutLayout>
+            <LayoutBox>
+                <Sidebar />
+                <Outlet />
+            </LayoutBox>
+        </LayoutLayout>
     );
 }
